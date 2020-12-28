48bcda0c223af79aab05419948666c5b31462e4b
EkjyotSingh <ekjyot091@gmail.com>
2020-12-28 16:11:14 +0530

    Https for css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Channel;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+                    }
         View::share('channels',Channel::with('discussions')->get());
     }
 }
