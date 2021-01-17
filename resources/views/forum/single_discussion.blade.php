36b663531d650268f670cc1a8f1713f2cd1867c6
EkjyotSingh <ekjyot091@gmail.com>
2021-01-17 13:53:59 +0530

    g

--- resources/views/forum/single_discussion.blade.php
+++ resources/views/forum/single_discussion.blade.php
@@ -151,7 +151,6 @@
 @endsection
 @section('script')
     <script type="text/javascript" src="{{asset('js/trix.js')}}"></script>
-    <script type="module" defer  src="{{asset('js/highlight.js')}}"></script>
     <script type="text/javascript">
         document.addEventListener('DOMContentLoaded', (event) => {
         document.querySelectorAll('pre').forEach((block) => {
