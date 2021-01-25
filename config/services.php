b4eb36a9347ef501c1a52d700b3253b4fed42436
EkjyotSingh <ekjyot091@gmail.com>
2021-01-25 18:56:03 +0530

    github oauth fix

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'https://ekjyotforum.herokuapp.com/login/github/callback',
+        'redirect' => env('GITHUB_CLIENT_CALLBACK_URL'),
     ],
 
 ];
